cd4db441000da682c4b5e44a0852669a09698021
shriwasshubham <61614458+shriwasshubham@users.noreply.github.com>
2023-06-19 08:32:28 +0530

    added commets in class

--- src/main/java/com/java/springrest/CloudHubRestApplication.java
+++ src/main/java/com/java/springrest/CloudHubRestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudHubRestApplication {
 
+
+	//This is My Application
 	public static void main(String[] args) {
 		SpringApplication.run(CloudHubRestApplication.class, args);
 	}
